784a935509aec9894fb51da01f898f95a9574b4d
Caprague <2210134086@qq.com>
2026-04-16 23:09:56 +0000

    Try AdamW Optimizer

--- examples/lidar_sparse_completion.py
+++ examples/lidar_sparse_completion.py
@@ -83,7 +83,6 @@ parser.add_argument("--save_freq_iter",     type=int,                   default=
 parser.add_argument("--batch_size",         type=int,                   default=8)
 parser.add_argument("--lr",                 type=float,                 default=1e-3)
 parser.add_argument("--weight_decay",       type=float,                 default=1e-4)
-parser.add_argument("--betas",              type=tuple(float, float),   default=(0.9, 0.995))
 parser.add_argument("--alpha",              type=float,                 default=0.8)
 parser.add_argument("--num_workers",        type=int,                   default=4)
 parser.add_argument("--log_dir",            type=str,                   default="./output/logs")
@@ -1049,7 +1048,7 @@ def train(net, dataloader, device, config):
         net.parameters(),
         lr=config.lr,                    
         weight_decay=config.weight_decay,          
-        betas=config.betas,         
+        betas=(0.9, 0.995),         
     )
 
     # 初始化损失函数
